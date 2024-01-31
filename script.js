45966d9b4098777cba86e42a83bda3d66536ca6d
kskannandotwin <kskannan.win@gmail.com>
2024-01-31 19:28:43 +0530

    31.01.2024-cc1
    completed

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-const measureKelvin = function () {
-  const measurement = {
-    type: "temp",
-    unit: "celsius",
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
 
-    // C) FIX
-    // value: Number(prompt('Degrees celsius:')),
-    value: 10,
-  };
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
 
-  // B) FIND
-  console.table(measurement);
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
 
-  // console.log(measurement.value);
-  // console.warn(measurement.value);
-  // console.error(measurement.value);
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
 
-  const kelvin = measurement.value + 273;
-  return kelvin;
-};
-// A) IDENTIFY
-console.log(measureKelvin());
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
 
-// Using a debugger
-const calcTempAmplitudeBug = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// - What is the X days? Answer: index + 1
 
-  let max = 0;
-  let min = 0;
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
   }
-  console.log(max, min);
-  return max - min;
+  console.log("..." + str);
 };
-const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
-// A) IDENTIFY
-console.log(amplitudeBug);
+printForecast(data1);
